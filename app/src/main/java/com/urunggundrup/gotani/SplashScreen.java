3f3551d2fab6595e6cafb07c830c39c4af7b721f
michaelloi <loimichael0@gmail.com>
2021-08-29 20:37:38 +0700

    all

--- app/src/main/java/com/urunggundrup/gotani/SplashScreen.java
+++ app/src/main/java/com/urunggundrup/gotani/SplashScreen.java
@@ -2,6 +2,7 @@ package com.urunggundrup.gotani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent a = new Intent(SplashScreen.this, MainActivity.class);
+                    a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(a);
+                }
+            }
+        };
+        timerThread.start();
     }
 }
